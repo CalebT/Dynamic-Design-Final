bca0241901ed008d020afc078d93a6e90a2d1dd0
Adam Fleming <adamfleming@Adams-Air.hitronhub.home>
2020-04-14 15:11:13 -0700

    partially completed sidebar/menu

---
+++ comps/Menu/menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './menu.css'
+
+const SidebarMenu = () => 
+
+<div className='entire_menu'>
+    <div className='menu_buttons'>
+        <button>Home</button>
+        <button>Tutorial</button>
+        <button>World Map</button>
+    </div>
+
+    <div className='menu_icon'>
+        Menu Icon
+    </div>
+</div>
+
+
+
+export default SidebarMenu
---
+++ stories/Menu.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SidebarMenu from '../comps/Menu/menu';
+
+export default {
+    title: 'SidebarMenu',
+    component: SidebarMenu
+}
+
+export const MySidebarMenu = () => <SidebarMenu />
