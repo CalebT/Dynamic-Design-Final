c0384beca71fcd93be05182af3bc2b266d391c00
Adam Fleming <adamfleming@Adams-Air.hitronhub.home>
2020-04-28 14:34:18 -0700

    finished world map

--- comps/Menu/menu.js
+++ comps/Menu/menu.js
@@ -1,43 +1,48 @@
-import React from 'react'
-import './menu.css'
-import BigButton from '../big-buttons'
-import {TiThMenu} from 'react-icons/ti'
-
-var menu_state = false;
-
-function MenuToggle(){
-    if(menu_state === false){
-        OpenMenu();
-    } else{
-        CloseMenu();
-    }
-}
-
-function OpenMenu(){
-    document.querySelector('.menu_buttons').style.left = '0px'
-    document.querySelector('.menu_icon').style.left = '0px;'
-    menu_state = true;
-}
-function CloseMenu(){
-    document.querySelector('.menu_buttons').style.left = '-100%'
-    document.querySelector('.menu_icon').style.left = '-250px'
-    menu_state = false;
-}
+import {useState} from 'react';
+import React from 'react';
+import './menu.css';
+import BigButton from '../big-buttons';
+import {TiThMenu} from 'react-icons/ti';
 
 
-const SidebarMenu = () => 
+const SidebarMenu = () => {
 
-<div className='entire_menu'>
-    <div className='menu_buttons'>
+const [menu_state, setMenu] = useState('-204px')
+
+return <div className='entire_menu'>
+    <div className='menu_buttons' style={{left:menu_state}}>
         <BigButton text='Home'/>
         <BigButton text='Tutorial'/>
         <BigButton text='World Map'/>
     </div>
-
     <div className='menu_icon'>
-        <TiThMenu onClick={MenuToggle} size={40}/>
+        <TiThMenu size={40} onClick={()=>{
+            
+            
+        }} />
     </div>
 </div>
-
+}
 
 export default SidebarMenu
+
+// var menu_state = false;
+
+// function MenuToggle(){
+//     if(menu_state === false){
+//         OpenMenu();
+//     } else{
+//         CloseMenu();
+//     }
+// }
+
+// function OpenMenu(){
+//     document.querySelector('.menu_buttons').style.left = '0px'
+//     document.querySelector('.menu_icon').style.left = '0px;'
+//     menu_state = true;
+// }
+// function CloseMenu(){
+//     document.querySelector('.menu_buttons').style.left = '-100%'
+//     document.querySelector('.menu_icon').style.left = '-250px'
+//     menu_state = false;
+//}
--- comps/WorldMap/worldmapcomp.js
+++ comps/WorldMap/worldmapcomp.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './worldmapcomp.css'
 
 const America = require('../../Imgs/Americas.svg');
+const Asia = require('../../Imgs/Asia.svg');
+const Africa = require('../../Imgs/Africa.svg');
+const Australia = require('../../Imgs/Australia.svg');
+const gray1 = require('../../Imgs/gray1.svg');
+const gray2 = require('../../Imgs/gray2.svg');
 
-const Americaimg = ({img}) => <div className='worldmap_img'>
-    <img src={img} />
+const Americaimg = ({img, img1, img2, img3, img4, img5, onClick}) => 
+<div className='worldmap_img'>
+    <img className ='america'src={img} onClick={onClick}/>
+    <img className ='asia' src={img1} onClick={onClick}/>
+    <img className ='africa' src={img2} onClick={onClick}/>
+    <img className ='australia' src={img3} onClick={onClick}/>
+    <img className ='gray1' src={img4} />
+    <img className ='gray2' src={img5} />
 </div>
 
 Americaimg.defaultProps = {
-    img: America
+    img: America,
+    img1: Asia,
+    img2: Africa,
+    img3: Australia,
+    img4: gray1,
+    img5: gray2,
+    onClick:'',
 }
 
 export default Americaimg;
