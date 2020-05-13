603d8cef3325849d7895f19877198f6de1489e26
Caleb <cteerling@gmail.com>
2020-05-12 22:52:59 -0700

    added info to index page before app

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () =>
     <div>
         <h1>Welcome</h1>
         <p>To start, we recommend using Google Chrome, and right clicking on the webpage and selecting "Inspect", then at the top on the page, set the size to Iphone X, as that is the size this version of the app works best with</p>
-        <p>Click <button href="/Homepage">Here</button> to check out Anipals</p>
+        <p>Click <a className="button" href="/Homepage">Here</a> to check out Anipals</p>
     </div>
 
 export default IndexPage;
