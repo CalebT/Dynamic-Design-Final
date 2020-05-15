488eb625c4a7b937a2bd5e694e88b9f1b6beee2b
Caleb <cteerling@gmail.com>
2020-05-15 15:40:38 -0700

    Added Ke-ju's updated homepage styles

--- pages/Homepage/index.js
+++ pages/Homepage/index.js
@@ -3,6 +3,9 @@ import './homepage.css'
 import AnipalsLogo from '../../comps/Logo/logo'
 import BigButton from '../../comps/big-buttons/index'
 import Router from 'next/router';
+import SidebarMenu from '../../comps/Menu/index';
+import Header from '../../comps/Header'
+
 
 function ClickStart() {
     Router.push("/Worldmappage")
@@ -14,11 +17,21 @@ function ClickTutorial() {
 
 const Homepage = () => 
 <div className='entire_home'>
+<SidebarMenu/>
     <div className='logo'>
         <AnipalsLogo />
     </div>
+    <div className='welcomeheader'>
+        <Header text="W" color="#FF5733"/>
+        <Header text="E" color="#FF9933"/>
+        <Header text="L" color="#F4F729"/>
+        <Header text="C" color="#51F325"/>
+        <Header text="O" color="#25F3F3"/>
+        <Header text="M" color="#2579F3"/>
+        <Header text="E" color="#7925F3"/>
+    </div>
     <div className='buttons'>
-        <BigButton text='Start'
+        <BigButton text='Let&apos;s Start!'
         onClick={ClickStart}/>
         <BigButton text='Tutorial'
         onClick={ClickTutorial}/>
